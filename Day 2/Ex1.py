c7e8d1bf8ffa44fa79e74e6cbc5947d7599d8594
Urvi Haval <80302137+quantumbluee@users.noreply.github.com>
2023-01-21 11:18:10 +0530

    Create Ex1.py

---
+++ Day 2/Ex1.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+two_digit_number_str = str(two_digit_number)
+sums = int(two_digit_number_str[0])+ int(two_digit_number_str[1])
+print(sums)
