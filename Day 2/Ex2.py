8f40fc9eadae530b4bbeec37ee2e3a1b6262b343
Urvi Haval <80302137+quantumbluee@users.noreply.github.com>
2023-01-21 11:27:29 +0530

    Create Ex2.py

---
+++ Day 2/Ex2.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+height_int = float(height)
+weight_int = float(weight)
+BMI = weight_int/(height_int**2)
+print(int(BMI))
